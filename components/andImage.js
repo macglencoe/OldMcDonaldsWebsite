050f58684a9a6d24f61cc908532aced8271e1011
Liam - INSPIRON <mcpaul1694@gmail.com>
2025-05-14 10:40:32 -0400

    styling for AndImage

--- components/andImage.js
+++ components/andImage.js
@@ -4,8 +4,11 @@ import styles from './andImge.module.css'
 export const AndImage = ({ src, children }) => {
     return (
         <div className={styles.andImage}>
-            <div>
-                {children}
+            <div className={styles.backdrop}>
+
+                <div className={styles.content}>
+                    {children}
+                </div>
             </div>
             <img src={src}></img>
         </div>
