7db3eefbc597e14d5b33b35b5370e6e9c44c2e05
Liam - INSPIRON <mcpaul1694@gmail.com>
2025-05-14 10:06:36 -0400

    added nature trails page

--- app/activities/page.js
+++ app/activities/page.js
@@ -52,7 +52,7 @@ export default function Activities() {
                     </div>
                     <div className={styles.largeActivity}>
                         <img className={styles.backdrop} src="https://plus.unsplash.com/premium_photo-1665311552973-53cdaeaa52c3?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
-                        <a href="/nature-trails"><h2 className={styles.light}>Nature Trails</h2></a>
+                        <a href="activities/nature-trails"><h2 className={styles.light}>Nature Trails</h2></a>
                     </div>
                     
                 </div>
