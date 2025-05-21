fd1d5ca1bb7f8aad7f6599dbb904f50aa59651bb
Liam - INSPIRON <mcpaul1694@gmail.com>
2025-05-21 13:30:01 -0400

    externalizing svgs and icon factory

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,15 @@ const nextConfig = {
                 hostname: 'images.unsplash.com',
             }
         ]
-    }
+    },
+    webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      resourceQuery: /raw/,        // only when you append `?raw`
+      type: 'asset/source',        // emit the raw file contents
+    })
+    return config
+  }
 };
 
 export default nextConfig;
---
+++ public/lib/icons.js
@@ -0,0 +1,41 @@
+import L from 'leaflet';
+import styles from '@/components/photoOpsMap.module.css';
+
+import entrance from '../icons/entrance.svg?raw';
+import photoOp from '../icons/photoOp.svg?raw';
+import tractor from '../icons/tractor.svg?raw';
+import animal from '../icons/animal.svg?raw';
+import bathroom from '../icons/bathroom.svg?raw';
+import food from '../icons/food.svg?raw';
+import football from '../icons/football.svg?raw';
+import location from '../icons/location.svg?raw';
+import playset from '../icons/playset.svg?raw';
+import flower from '../icons/flower.svg?raw';
+import maze from '../icons/maze.svg?raw';
+
+export function makeIcon({ svg, className = styles.marker, size = 24, anchor = size / 2 }) {
+    console.log(svg)
+    return new L.DivIcon({
+        className: className,
+        html: `<div>${svg}</div>`,
+        iconSize: [size, size],
+        iconAnchor: [anchor, anchor],
+        popupAnchor: [0, -anchor]
+    })
+}
+
+export const icons = {
+    entrance: makeIcon({ svg: entrance, className: styles.marker + ' ' + styles.t, size: 30 }),
+    photoOp: makeIcon({ svg: photoOp }),
+    tractor: makeIcon({ svg: tractor }),
+    animal: makeIcon({ svg: animal }),
+    bathroom: makeIcon({ svg: bathroom }),
+    food: makeIcon({ svg: food }),
+    football: makeIcon({ svg: football }),
+    location: makeIcon({ svg: location }),
+    playset: makeIcon({ svg: playset, className: styles.marker + ' ' + styles.t, size: 30 }),
+    flower: makeIcon({ svg: flower }),
+    maze: makeIcon({ svg: maze })
+}
+
+export default icons;
