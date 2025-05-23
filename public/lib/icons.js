b230cbbf77921948abec7d2d6b2a03e818fa3438
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-05-23 17:31:13 -0400

    moved icon implementation to phosphor library

--- public/lib/icons.js
+++ public/lib/icons.js
@@ -13,6 +13,9 @@ import playset from '../icons/playset.svg?raw';
 import flower from '../icons/flower.svg?raw';
 import maze from '../icons/maze.svg?raw';
 import clearing from '../icons/clearing.svg?raw';
+import arrowLeft from '../icons/arrow-left.svg?raw';
+import arrowSquareOut from '../icons/arrow-square-out.svg?raw';
+import map from '../icons/map.svg?raw';
 
 export function makeIcon({ svg, className = styles.marker, size = 24, anchor = size / 2 }) {
     console.log(svg)
@@ -37,7 +40,10 @@ export const icons = {
     playset: makeIcon({ svg: playset, className: styles.marker + ' ' + styles.t, size: 30 }),
     flower: makeIcon({ svg: flower }),
     maze: makeIcon({ svg: maze }),
-    clearing: makeIcon({ svg: clearing })
+    clearing: makeIcon({ svg: clearing }),
+    arrowLeft: makeIcon({ svg: arrowLeft }),
+    arrowSquareOut: makeIcon({ svg: arrowSquareOut }),
+    map: makeIcon({ svg: map })
 }
 
 export default icons;
