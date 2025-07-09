e644782a13bc8f7be40756255630529a8b7d823f
Liam - INSPIRON <southernscotts112@gmail.com>
2025-07-09 17:27:44 -0400

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,45 @@ const nextConfig = {
             }
         ]
     },
+    async redirects() {
+      return [
+        {
+          source: '/night-maze',
+          destination: '/activities/night-maze',
+          permanent: true
+        },
+        {
+          source: '/rates-hours',
+          destination: '/',
+          permanent: true
+        },
+        {
+          source: '/about-the-farm-1',
+          destination: '/about',
+          permanent: true
+        },
+        {
+          source: '/birthday-parties-1',
+          destination: '/reservations',
+          permanent: true
+        },
+        {
+          source: '/corn-maze',
+          destination: '/activities/corn-maze',
+          permanent: true
+        },
+        {
+          source: '/pumpkin-patch',
+          destination: '/activities/pumpkin-patch',
+          permanent: true
+        },
+        {
+          source: '/location',
+          destination: '/visit',
+          permanent: true
+        }
+      ]
+    },
     webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
