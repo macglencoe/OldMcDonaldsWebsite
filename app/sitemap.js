8a57ba46c04369f3aeb68603b73b4e70bc8aff67
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-07-16 12:45:48 -0400

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,78 @@
+
+
+export default function sitemap() {
+    return [
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1.0,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/visit',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1.0,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1.0,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/about',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 5.0,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/faq',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/reservations',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities/corn-maze',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities/flower-fields',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities/hayrides',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities/nature-trails',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities/night-maze',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://oldmcdonaldspumpkinpatchwv.com/activities/pumpkin-patch',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+    ]
+}
