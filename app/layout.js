2d0a3638a789f547a6da4e5ac1613f350f2815d6
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-07-21 23:39:30 -0400

    add client-side cookie notice and integrate into layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import CookieNotice from "@/components/cookieNotice";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +24,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
+        <CookieNotice />
         {children}
       </body>
     </html>
