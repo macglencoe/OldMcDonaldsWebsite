4071cf934c8898fbe3ac4f1c6528e5c3b185c99a
Liam - Latitude <southernscotts112@gmail.com>
2025-08-10 09:27:29 -0400

    added backdrop image with blur and gradient

--- components/home/farmSwapBanner.js
+++ components/home/farmSwapBanner.js
@@ -7,16 +7,24 @@ export default function FarmSwapBanner() {
     const eventURL = "https://facebook.com"
     const subtitle = "Crafts - Stuff - Placeholder - Words"
     return (
-        <div className="py-9 px-2 md:px-5 flex flex-col md:flex-row items-center flex-wrap bg-foreground text-background">
-            <div className="w-fit text-sm md:text-5xl flex flex-col gap-1 md:max-w-none max-w-[280px]">
-                <p className="border-b-2 pb-1 md:pb-2"><strong>{date}</strong> - Come visit our <strong className="font-satisfy">Farm Swap</strong></p>
-                <p className="text-center md:text-left text-md md:text-2xl">{subtitle}</p>
-            </div>
-            <div className="flex-1 flex items-center justify-center w-fit">
-                <a href={eventURL} target="_blank" className="flex items-center bg-accent w-fit py-1 px-2 md:py-2 md:px-3 my-2 text-background md:text-lg text-sm font-bold rounded md:rounded-xl">
-                    More info
-                    &nbsp;<FacebookLogo size={24} weight="bold" alt="Facebook" className="inline-block" />
-                </a>
+        <div className="bg-foreground " style={{
+            backgroundImage: "url(/picnicTable.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+        }}>
+            <div className="backdrop-blur-sm py-9 px-2 md:px-5 flex flex-col md:flex-row items-center flex-wrap text-background bg-foreground/50" style={{
+                backgroundImage: "linear-gradient(var(--foreground) 0%, transparent 30%, transparent 60%, var(--foreground) 100%)"
+            }}>
+                <div className="w-fit text-sm md:text-5xl flex flex-col gap-1 md:max-w-none max-w-[280px]">
+                    <p className="border-b-2 pb-1 md:pb-2"><strong>{date}</strong> - Come visit our <strong className="font-satisfy">Farm Swap</strong></p>
+                    <p className="text-center md:text-left text-md md:text-2xl">{subtitle}</p>
+                </div>
+                <div className="flex-1 flex items-center justify-center w-fit">
+                    <a href={eventURL} target="_blank" className="flex items-center bg-accent w-fit py-1 px-2 md:py-2 md:px-3 my-2 text-background md:text-lg text-sm font-bold rounded md:rounded-xl">
+                        More info
+                        &nbsp;<FacebookLogo size={24} weight="bold" alt="Facebook" className="inline-block" />
+                    </a>
+                </div>
             </div>
         </div>
     )
