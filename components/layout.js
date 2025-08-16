e341638e76ab9a1c7f428c47162672ee35bc346a
Liam - Latitude <southernscotts112@gmail.com>
2025-08-15 21:02:27 -0400

    added floating nav

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import AuxiliaryFAQ from "./auxiliaryFAQ"
+import FloatingNav from "./floatingNav"
 import Footer from "./footer"
 import { Analytics } from "@vercel/analytics/next"
 
@@ -7,7 +8,8 @@ const { default: Navbar } = require("./navbar")
 
 const Layout = (props) => {
     return (
-        <div>
+        <div className="relative">
+            <FloatingNav />
             <Navbar />
             <Analytics />
             <div className="overflow-x-scroll">
