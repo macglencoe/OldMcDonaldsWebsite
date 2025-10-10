3746939a2829442d51a868b88ee1cfaacf64a2a4
macglencoe - TODDTOWERD <mcpaul1694@gmail.com>
2025-10-10 10:28:34 -0400

    night maze pricing update

--- app/activities/night-maze/page.js
+++ app/activities/night-maze/page.js
@@ -22,18 +22,33 @@ export const NightMaze = () => {
                     </AndImage>
                     <AndImage style="night" src="/bonfires.jpg">
                         <h2>Pricing</h2>
-                        <p>For one person, above 3 years of age:</p><p className="big">$7</p>
-                        <p>Must be paid at the admission booth</p>
-                    </AndImage>
-                    <AndImage fromUnsplash style="night" src="https://images.unsplash.com/photo-1520857622823-2df34928e183?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-                        <h2>Includes:</h2>
-                        <ul>
-                            <li>Night Maze</li>
-                            <li>Night Hayride</li>
-                            <li>Campfires</li>
-                            <li>Corn Crib</li>
-                            <li>Access to vendors</li>
-                        </ul>
+                        <div className="rounded-lg overflow-hidden">
+                            <div className="p-4 bg-foreground/20">
+                                <h3 className="font-bold !font-serif">Admission:</h3>
+                                <p><span className="text-7xl font-bold">$7</span>, per person*</p>
+                                <p className="italic mt-3">Includes:</p>
+                                <ul>
+                                    <li>Night Maze</li>
+                                    <li>Campfires</li>
+                                    <li>Playground</li>
+                                    <li>Access to market & vendors</li>
+                                </ul>
+                            </div>
+                            <hr className="border-background/30" />
+                            <div className="p-4 bg-foreground/40">
+                                <h3 className="font-bold !font-serif">Admission + Hayride:</h3>
+                                <p><span className="text-7xl font-bold">$10</span>, per person*</p>
+                                <p className="italic mt-3">Includes:</p>
+                                <ul>
+                                    <li>Everything above</li>
+                                    <li>1 hayride ticket</li>
+                                </ul>
+                            </div>
+                            <div className="p-4 bg-foreground/60">
+                                <p className="text-center">Must be paid at the admission booth</p>
+                            <p className="!text-sm text-center">*Children 3 and under are free</p>
+                            </div>
+                        </div>
                     </AndImage>
                 </div>
             </Layout>
