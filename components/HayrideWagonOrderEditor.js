fc139723bcf1e4d31344298efd16a90a5bdd2dc7
Liam - Latitude <southernscotts112@gmail.com>
2025-10-12 14:50:06 -0400

    brighter colors

--- components/HayrideWagonOrderEditor.js
+++ components/HayrideWagonOrderEditor.js
@@ -20,21 +20,21 @@ import { CSS } from "@dnd-kit/utilities";
 import { CaretDown, DotsSixVertical } from "phosphor-react";
 
 const BG_COLOR_CLASSES = {
-    amber: "bg-amber-600",
-    blue: "bg-blue-600",
-    emerald: "bg-emerald-600",
-    green: "bg-green-600",
-    indigo: "bg-indigo-600",
-    lime: "bg-lime-600",
-    orange: "bg-orange-600",
-    pink: "bg-pink-600",
-    purple: "bg-purple-600",
-    red: "bg-red-600",
-    rose: "bg-rose-600",
-    sky: "bg-sky-600",
-    teal: "bg-teal-600",
-    violet: "bg-violet-600",
-    yellow: "bg-yellow-600",
+    amber: "bg-amber-500",
+    blue: "bg-blue-500",
+    emerald: "bg-emerald-500",
+    green: "bg-green-500",
+    indigo: "bg-indigo-500",
+    lime: "bg-lime-500",
+    orange: "bg-orange-500",
+    pink: "bg-pink-500",
+    purple: "bg-purple-500",
+    red: "bg-red-500",
+    rose: "bg-rose-500",
+    sky: "bg-sky-500",
+    teal: "bg-teal-500",
+    violet: "bg-violet-500",
+    yellow: "bg-yellow-500",
 };
 
 function DragHandleButton({ listeners, label, disabled }) {
