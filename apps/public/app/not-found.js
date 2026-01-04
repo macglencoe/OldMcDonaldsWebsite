04cbd89938a8a2e281dde92fbc00d169907156cc
macglencoe - TODDTOWERD <mcpaul1694@gmail.com>
2026-01-04 18:07:36 -0500

    fix(lint): <Link> instead of <a>

--- apps/public/app/not-found.js
+++ apps/public/app/not-found.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
     return (
@@ -13,7 +14,7 @@ export default function NotFound() {
                         <h1 className="text-7xl font-bold text-white">404</h1>
                         <h1 className="text-3xl font-bold text-white">Lost on the Farm</h1>
                         <p className="text-white">There&apos;s nothing to do out here</p>
-                        <p><a href="/" className="text-background bg-accent/50 px-5 py-2 rounded-full hover:bg-accent">Go Home</a></p>
+                        <p><Link href="/" className="text-background bg-accent/50 px-5 py-2 rounded-full hover:bg-accent">Go Home</Link></p>
                         <p><a href="/contact#dev" className="text-background bg-foreground/50 px-5 py-2 rounded-full hover:bg-foreground">Contact Dev</a></p>
                     </div>
                 </div>
