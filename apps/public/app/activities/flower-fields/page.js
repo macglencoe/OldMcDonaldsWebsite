8e660486bc0d9ffc10ce193cb91290da73fecd7e
macglencoe - TODDTOWERD <mcpaul1694@gmail.com>
2026-01-04 12:08:14 -0500

    monorepo refactor

---
+++ apps/public/app/activities/flower-fields/page.js
@@ -0,0 +1,40 @@
+import { Action } from "@oldmc/ui";
+import { AndImage } from "@/components/andImage";
+import Layout from "@/components/layout";
+import PageHeader from "@/components/pageHeader";
+import pricing from "@/public/data/pricing";
+
+export const metadata = {
+    title: "Flower Fields",
+    description: "Pick your own sunflowers, zinnias, and cosmos at Old McDonald’s Pumpkin Patch. Visit our flower fields, cut your own bouquet, and capture farm photos."
+}
+
+export default function FlowerFields() {
+    return (
+        <Layout>
+            <PageHeader subtitle="2025 Season">Flower Fields</PageHeader>
+            <div className="body basic">
+                <AndImage src="/sunflowerCloseUp.jpg">
+                    <h2>Sunflowers, Cosmos, & Zinnias</h2>
+                    <p>Take a stroll out to one of our 1-acre flower fields</p>
+                    <p><a href="#cut-your-own">Cut your own</a>, take some pictures, or just enjoy</p>
+                    <Action as="Link" className={'mx-auto'} href='/map?x=39.38163138370034&y=-78.04489454865366' variant="primary">Find on the Map</Action>
+
+                </AndImage>
+                <AndImage src="/flowerbar.jpg">
+                    <h2 id="cut-your-own">Cut your own</h2>
+                    <p>We have an arrangement station ("Flower Bar") near the flower fields, where you will find cutters and cups</p>
+                    <ul>
+                        <li>For <b>one cup</b> of <b>Any Flower</b>:<br></br><p className="big">${pricing["flower-cup"].amount.toFixed(2)}</p></li>
+                    </ul>
+                    <b>PLEASE RETURN CUTTERS TO ARRANGEMENT STATION</b>
+                </AndImage>
+                <AndImage src="/flowerbarInterior.jpg">
+                    <h2>Grab a vase too!</h2>
+                    <p>We have a limited selection of glass vases available at the arrangement station</p>
+                    <p>Each vase is priced individually</p>
+                </AndImage>
+            </div>
+        </Layout>
+    )
+}
---
+++ apps/public/app/activities/hayrides/page.js
@@ -0,0 +1,50 @@
+import Layout from '@/components/layout'
+import styles from './page.module.css'
+import { AndImage } from '@/components/andImage'
+import pricing from '@/public/data/pricing'
+import PageHeader from '@/components/pageHeader'
+import { Action } from "@oldmc/ui";
+
+export const metadata = {
+    title: "Hay Rides",
+    description: "Enjoy scenic hayrides at Old McDonald’s Pumpkin Patch. Take a 20-minute tour of the farm by wagon, with special night hayrides in October."
+}
+
+export const HayRide = () => {
+    return (
+        <Layout>
+            <PageHeader subtitle="2025 Season">Hay Rides</PageHeader>
+            <div className='body basic'>
+                <AndImage src='/hillview.jpg'>
+                    <h2>A tour of the farm</h2>
+                    <p>Relax on a 20-minute haywagon ride with scenic views of the property</p>
+                </AndImage>
+                <AndImage src='/pondeastdock.jpg'>
+                    <h2>Pricing</h2>
+                    <p>For one person, above 3 years of age:</p>
+                    <p className='big'>${pricing.hayride.amount}</p>
+                    <p>Must be paid at the admission booth</p>
+                    <p>If you plan on bringing a large group, make sure your group is all together at the admission booth</p>
+                </AndImage>
+                <AndImage src='/hayrideGroupPhoto.jpg'>
+                    <h2>When to get on</h2>
+                    <p>Each ticket has a <b>color</b> and a <b>time</b> written on the back</p>
+                    <p>Arrive <b>before</b> the time on your ticket.</p>
+                    <p>When your time comes, go to the hayride corral, which is across the creek</p>
+                    <Action as='Link' href='/map?x=39.382529281329774&y=-78.04355774914931' variant='primary' className={'mx-auto'}>Find on the Map</Action>
+                    <p>The drivers will direct you to the correct wagon, and take your ticket</p>
+                </AndImage>
+                <AndImage fromUnsplash style="night" src="https://images.unsplash.com/photo-1707755939969-e9c1da71c5bb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+                    <div className={styles.nightMaze}>
+                        <h2>Hayrides at night</h2>
+                        <p>Starting October 17th, we will have hayrides open from 7:30pm to 10pm</p>
+                        <p>Bring your friends and some warm clothes for a spooky tour of the deep dark forest at night</p>
+                        <Action as='Link' href='/activities/night-maze' className={'mx-auto'} variant='secondary'>See More</Action>
+                    </div>
+                </AndImage>
+            </div>
+        </Layout>
+    )
+}
+
+export default HayRide
